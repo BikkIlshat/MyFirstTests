e5b791c58e8b8b680439099c40983eedb2c27b35
BikkIlshat <HeavyIlshat@rambler.ru>
2022-03-24 23:50:25 +0300

    Заменил текущее решение, основанное на Presenter на ViewModel

--- app/src/main/java/com/geekbrains/myfirsttests/utils/SchedulerProvider.kt
+++ app/src/main/java/com/geekbrains/myfirsttests/utils/SchedulerProvider.kt
@@ -2,7 +2,7 @@ package com.geekbrains.myfirsttests.utils
 
 import io.reactivex.Scheduler
 
-internal interface SchedulerProvider {
+interface SchedulerProvider {
     fun ui(): Scheduler
     fun io(): Scheduler
 }
